62bb023d9db4d16f21c1cf2fc4b2ee844d83ee9a
owhenrique <me.pauloalmeida@gmail.com>
2024-05-13 00:31:56 -0300

    feat: add workout page

---
+++ src/mocks/aluno.ts
@@ -0,0 +1,12 @@
+import { Aluno } from "@/interfaces/types";
+
+const aluno: Aluno = {
+    foto: "https://raw.githubusercontent.com/owhenrique/workout-plan-carla-costa/main/public/avatar.jpeg",
+    sigla: "CC",
+    nome: "Carla Costa",
+    idade: "57",
+    peso: "68",
+    objetivo: "Emagrecimento"
+}
+
+export default aluno;
